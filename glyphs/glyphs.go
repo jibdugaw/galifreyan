e6cb281e9ab0d705a25774aeb5c42ceec19593be
John Dugaw <jibdugaw@yahoo.com>
2018-03-11 10:23:19 -0700

    Deleted a lot of dead code from conversion to Radians. Refactored Colloquy and Glyphs to  have better naming convention, and moved rendering from package main to glyphs where it belongs.

--- glyphs/glyphs.go
+++ glyphs/glyphs.go
@@ -1,7 +1,11 @@
 package glyphs
 
 import (
+	"fmt"
+	"math"
 	"strings"
+
+	"github.com/fogleman/gg"
 )
 
 const (
@@ -11,15 +15,19 @@ const (
 	on
 )
 
+// RADIANS defines the number of RADIANS comprising a circle
+// fractions of this value are used throughout
+const RADIANS float64 = 2 * math.Pi
+
 // Glyph represents the galifrayen transliteration of a character
 type Glyph struct {
 	Owner      *Colloquy // used to find relative position
-	XPos       float64   // TODO - Renderer knows positioning
-	YPos       float64   // TODO - Renderer knows positioning
+	X          float64   // needed to tell composition Glyphs where to go
+	Y          float64   // needed to tell composition Glyphs where to go
+	Radius     float64   //
+	Angle      float64   // for non-combining glyphs where do they go on the Glyph
 	Word       string    // original word we received TODO - save on memory if we bypass this
 	Characters []*GlyphChar
-	Step       float64 // TODO - maybe Renderer
-	Radius     float64 // TODO - maybe renderer
 }
 
 // GlyphChar represents a single character to be converted into a Galifreyan word glyph
@@ -41,26 +49,22 @@ type Colloquy struct {
 }
 
 // NewColloquy creates a new Galifreyan sentence
-func NewColloquy(x0 float64, y0 float64, r float64, s float64, sent string, gs []*Glyph) *Colloquy {
-	ret := &Colloquy{x0, y0, r, s, sent, gs}
-	words := strings.Fields(sent)
-	glyphs := make([]*Glyph, len(words))
-	for word := range words {
-		g := NewGlyph(ret, float64(-1), float64(-1), words[word], float64(word), float64(-1))
-		glyphs[word] = g
-	}
-	ret.Glyphs = glyphs
+// x - x coordinate around which to draw
+// y - x coordinate around which to draw
+// r - radius used to draw colloquy
+// sentence - the sentence to be represented
+func NewColloquy(x0 float64, y0 float64, r float64, sentence string) *Colloquy {
+
+	ret := &Colloquy{x0, y0, r, 0, sentence, nil}
+	words := strings.Fields(sentence)
+	ret.Step = RADIANS / float64(len(words))
+
+	ret.Parse()
+
 	return ret
 }
 
-// NewGlyph returns a properly defaulted Glyph object
-func NewGlyph(c *Colloquy, x float64, y float64, w string, step float64, radius float64) *Glyph {
-	// split sentence by white space
-	g := &Glyph{c, x, y, w, make([]*GlyphChar, 2), step, radius}
-	g.parse()
-	return g
-}
-
+// parse parses a Glyph and seeds the GlyphChar
 func (g *Glyph) parse() {
 	midWord := true
 	for i := 0; i < len(g.Word); i++ {
@@ -114,4 +118,57 @@ func (g *Glyph) parse() {
 			break
 		}
 	}
+}
+
+// Draw iterates over Glyphs of a Coloquy and draws them to the provided canvas
+func (c *Colloquy) Draw(dc *gg.Context) {
+
+	for i := range c.Glyphs {
+		c.Glyphs[i].Draw(dc)
+	}
+
+}
+
+// Draw draws an individual Glyph
+func (g *Glyph) Draw(dc *gg.Context) {
+	fmt.Println("TODO - implement individual GlyphChar drawing")
+	dc.DrawCircle(g.X, g.Y, g.Radius)
+}
+
+// Parse will parse the Colloquy sentence and seed Glyphs
+func (c *Colloquy) Parse() {
+
+	words := strings.Fields(c.Sentence)
+	firstWordStart := .75 * RADIANS // TODO - verify
+	myStep := firstWordStart
+	c.Glyphs = make([]*Glyph, len(words))
+	for i := range words {
+		g := NewGlyph(c, words[i], myStep)
+		g.Parse()
+		c.Glyphs[i] = g
+		// update myStep and handle wrap arounds
+		myStep += c.Step
+		if myStep > RADIANS {
+			myStep -= 2 * RADIANS
+		}
+	}
+
+}
+
+// NewGlyph returns a properly defaulted Glyph object
+//func NewGlyph(c *Colloquy, x float64, y float64, w string, step float64, radius float64) *Glyph {
+func NewGlyph(c *Colloquy, w string, angle float64) *Glyph {
+	ret := &Glyph{}
+	ret.Owner = c
+	ret.Word = w
+	ret.Angle = angle
+	ret.Radius = 10 // TODO - this is a future BUG!
+	ret.X = c.X + float64(c.Radius)*math.Cos(ret.Angle)
+	ret.Y = c.Y + float64(c.Radius)*math.Sin(ret.Angle)
+	return ret
+}
+
+// Parse will parse the word assciated with the glyph seeding GlyphChars
+func (*Glyph) Parse() {
+	// TODO
 }
--- main.go
+++ main.go
@@ -3,45 +3,15 @@ package main
 // TODO - add placeholder circles for Glyphs at each step ont he perimeter of the Colloquy
 
 import (
-	"fmt"
-	"math"
-	"strings"
-
 	gglyphs "github.com/jibdugaw/galifreyan/glyphs"
 
 	"github.com/fogleman/gg"
 )
 
-// RADIANS defines the number of RADIANS comprising a circle
-// fractions of this value are used throughout
-const RADIANS float64 = 2 * math.Pi
-
-/*
-
-func main() {
-	const S = 1024
-	dc := gg.NewContext(S, S)
-	dc.SetRGBA(0, 0, 0, 0.1)
-	for i := 0; i < 360; i += 15 {
-		dc.Push()
-		dc.RotateAbout(gg.Radians(float64(i)), S/2, S/2)
-		dc.DrawEllipse(S/2, S/2, S*7/16, S/8)
-		dc.Fill()
-		dc.Pop()
-	}
-	dc.SavePNG("out.png")
-}
-*/
-
 func main() {
 	//input := "gallifrey falls no more"
 	input := "Doctor it is so much bigger on the inside"
-	words := strings.Fields(input)
-	wordCount := float64(len(words))
-	wordStep := RADIANS / wordCount
-	// MAGERY: Gallifreyan characters start at the 6 o'clock position when
-	// looking at a clock face, or 270º and run counter clockwise
-	firstWordStart := .75 * RADIANS
+	//input := "i am the doctor"
 
 	// our globals which will be the canvas size and then some basic starting point stuff
 	canvasSide := float64(1000)
@@ -49,108 +19,17 @@ func main() {
 	originY := canvasSide / 2
 
 	// TODO - retrieve sentences and create colloquy's for each
-	// TODO - parse sentence creating glyph holder for each word
-	// TODO - parse parse letters into glyph drawing profile
-	sentence := gglyphs.NewColloquy(originX, originY, float64(0.8)*originX, wordStep, "", make([]*gglyphs.Glyph, len(words)))
-	myStep := firstWordStart
-	for i := range words {
-		g := gglyphs.NewGlyph(sentence, -1.0, -1.0, words[i], myStep, 0.0)
-		sentence.Glyphs = append(sentence.Glyphs, g)
-		// update myStep and handle wrap arounds
-		myStep -= wordStep
-		if myStep < 0 {
-			myStep += 2 * RADIANS
-		}
-	}
+	sentence := gglyphs.NewColloquy(originX, originY, float64(0.8)*originX, input)
 
 	dc := gg.NewContext(1000, 1000)
 	// using originX, originY, radius and offsets for each glyph in the colloquy
 	// using originX, originY and radius of colloquy draw each word glyph
 	dc.SetLineWidth(10)
-	DrawColloquy(originX, originY, dc, sentence)
+	sentence.Draw(dc)
 	//DrawB(dc)
 	//dc.DrawArc(400, 325, 300, Radians(0), Radians(90))
 	//dc.SetRGB(255, 188, 55)
 	//dc.Fill()
 	dc.Stroke()
 	dc.SavePNG("out.png")
-}
-
-// Radians converts signed degrees into signed radians
-func Radians(degrees float64) float64 {
-	return (degrees * float64(math.Pi) / float64(180))
-}
-
-// DrawColloquy iterates over Glyphs of a Coloquy and draws them
-func DrawColloquy(x0 float64, y0 float64, dc *gg.Context, c *gglyphs.Colloquy) {
-	// iterate glyphs and draw those
-
-	for i := range c.Glyphs {
-		fmt.Printf("working on Glyph: %v\n", c.Glyphs[i])
-		deltaX := float64(c.Radius) * math.Cos(c.Glyphs[i].Step)
-		deltaY := float64(c.Radius) * math.Sin(c.Glyphs[i].Step)
-
-		switch {
-		case c.Glyphs[i].Step <= .25*RADIANS:
-			if deltaX < 0 {
-				deltaX = -deltaX
-			}
-			if deltaY > 0 {
-				deltaY = -deltaY
-			}
-			dc.SetRGB(255, 0, 0)
-			fmt.Printf("I think I'm in quadrant I (deltaX, deltaY) = (%v,%v)\n", deltaX, deltaY)
-			break
-		case c.Glyphs[i].Step <= .5*RADIANS:
-			if deltaX > 0 {
-				deltaX = -deltaX
-			}
-			if deltaY > 0 {
-				deltaY = -deltaY
-			}
-			dc.SetRGB(0, 255, 0)
-			fmt.Printf("I think I'm in quadrant II (deltaX, deltaY) = (%v,%v)\n", deltaX, deltaY)
-			fmt.Printf("(x,y) = (%v, %v)\n", float64(x0)+deltaX, float64(y0)+deltaY)
-			break
-
-		case c.Glyphs[i].Step <= .75*RADIANS:
-			if deltaX > 0 {
-				deltaX = -deltaX
-			}
-			if deltaY < 0 {
-				deltaY = -deltaY
-			}
-			dc.SetRGB(0, 0, 255)
-			fmt.Printf("I think I'm in quadrant III (deltaX, deltaY) = (%v,%v)\n", deltaX, deltaY)
-			fmt.Printf("(x,y) = (%v, %v)\n", float64(x0)+deltaX, float64(y0)+deltaY)
-			break
-
-		case c.Glyphs[i].Step <= RADIANS:
-			if deltaX < 0 {
-				deltaX = -deltaX
-			}
-			if deltaY < 0 {
-				deltaY = -deltaY
-			}
-			dc.SetRGB(0, 0, 0)
-			fmt.Printf("I think I'm in quadrant IV (deltaX, deltaY) = (%v,%v)\n", deltaX, deltaY)
-			fmt.Printf("(x,y) = (%v, %v)\n", float64(x0)+deltaX, float64(y0)+deltaY)
-			break
-		}
-
-		c.Glyphs[i].XPos = c.X + deltaX
-		c.Glyphs[i].YPos = c.Y + deltaY
-		if c.Glyphs[i].Radius < 10 {
-			c.Glyphs[i].Radius = 10
-		}
-		dc.DrawCircle(c.Glyphs[i].XPos, c.Glyphs[i].YPos, c.Glyphs[i].Radius)
-
-	}
-}
-
-// DrawB draws a Galifreyan B
-func DrawB(dc *gg.Context) {
-	dc.DrawArc(500, 500, 250, Radians(110), Radians(430)) //8.125)
-	dc.DrawArc(250, 250, 30, Radians(0), Radians(90))     //8.125)
-	//dc.
 }
